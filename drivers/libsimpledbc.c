cb6d7b1b2498d9e2f9982530e3821fd4bede7ef8
Navin Shrinivas <karupal2002@gmail.com>
2021-07-24 22:00:31 +0530

    improvements

--- drivers/libsimpledbc.c
+++ drivers/libsimpledbc.c
@@ -1,6 +1,35 @@
+/**********************************************************************************
+ ____  _                 _      ____  ____  
+/ ___|(_)_ __ ___  _ __ | | ___|  _ \| __ ) 
+\___ \| | '_ ` _ \| '_ \| |/ _ \ | | |  _ \ 
+ ___) | | | | | | | |_) | |  __/ |_| | |_) |
+|____/|_|_| |_| |_| .__/|_|\___|____/|____/ 
+                  |_|  
+Copyright (c) 2021 P K Navin Shrinivas
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**********************************************************************************/
+
 #include<stdio.h>
 #include<string.h>
+#if _WIN32
+#include"curl\curl.h"
+#else
 #include<curl/curl.h>
+#endif
 #include<ctype.h>
 
 void dbread(char* host , char* username, char* password , char* file)
@@ -20,24 +49,44 @@ void dbread(char* host , char* username, char* password , char* file)
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
     res = curl_easy_perform(curl);
     fclose(f);
-    char a[100000];
-    FILE* f1=fopen("new.txt","r");
-    FILE* f2=fopen(file,"w+");
-    char data[1000];
-    fscanf(f1,"%[^NULL]%*c",data);
-    char* split=data;
-    char* token1=strtok_r(split,"\n",&split);
-    while(token1!=NULL)
+    if(strcmp(file,"STDOUT")==0)
     {
-        if(strcmp(token1,"<pre>")!=0 && strcmp(token1,"</pre>")!=0)
+        char a[100000];
+        FILE* f1=fopen("new.txt","r");
+        char data[1000];
+        fscanf(f1,"%[^EOF]%*c",data);
+        char* split=data;
+        char* token1=strtok_r(split,"\n",&split);
+        while(token1!=NULL)
         {
-        fprintf(f2,"%s",token1);
-        fprintf(f2,"%s","\n");
+            if(strcmp(token1,"<pre>")!=0 && strcmp(token1,"</pre>")!=0)
+            {
+                printf("%s\n",token1);
+            }
+            token1=strtok_r(split,"\n",&split);
         }
-        token1=strtok_r(split,"\n",&split);
+        fclose(f1);
     }
-    fclose(f2);fclose(f1);remove("new.txt");
-    curl_easy_cleanup(curl);
+    else{
+        char a[100000];
+        FILE* f1=fopen("new.txt","r");
+        FILE* f2=fopen(file,"w+");
+        char data[1000];
+        fscanf(f1,"%[^EOF]%*c",data);
+        char* split=data;
+        char* token1=strtok_r(split,"\n",&split);
+        while(token1!=NULL)
+        {
+            if(strcmp(token1,"<pre>")!=0 && strcmp(token1,"</pre>")!=0)
+            {
+                fprintf(f2,"%s",token1);
+                fprintf(f2,"%s","\n");
+            }
+            token1=strtok_r(split,"\n",&split);
+        }
+        fclose(f2);fclose(f1);
+    }
+    curl_easy_cleanup(curl);remove("new.txt");
 }
 
 void dbwrite(char* host , char* username, char* password , char* data)
@@ -62,7 +111,6 @@ void dbwrite(char* host , char* username, char* password , char* data)
     strcat(link,password);
     strcat(link,"/");
     strcat(link,data);
-    // printf("%s",link);
     curl_easy_setopt(curl,CURLOPT_URL,link);
     curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
     printf("Test data successfully written to database!\n");
